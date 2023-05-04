954366b15709b23b2b0ddbf45d7152e206b97289
Jp Fortuno <jp.fortuno@nullpod.com>
2023-05-04 21:22:30 +0400

    refactor code

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,15 +2,19 @@ require "open-uri"
 
 class FlatsController < ApplicationController
     URL = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json".freeze
+    before_action :set_flats
 
     def index
-        @flats = JSON.parse(URI.open(URL).read)
     end
 
     def show
-        @flats = JSON.parse(URI.open(URL).read)
         id_to_search = params[:id].to_i
-
         @flat = @flats.find { |flat| flat["id"] == id_to_search }
+    end
+
+    private 
+
+    def set_flats
+        @flats = JSON.parse(URI.open(URL).read)
     end
 end
